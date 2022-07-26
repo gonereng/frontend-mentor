75a04730acc28dc6ef569960a3dc2192f780e52f
Roland Theisen <roland.theisen@paulwurth.com>
2022-07-26 11:09:48 +0200

    Initial commit

---
+++ product-preview-card-component-main/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        darkCyan: 'hsl(158, 36%, 37%)',
+        cream: 'hsl(30, 38%, 92%)',
+        veryDarkBlue: 'hsl(212, 21%, 14%)',
+        darkGrayishBlue: 'hsl(228, 12%, 48%)',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        fraunces: ['Fraunces', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
